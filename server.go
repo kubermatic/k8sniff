9dde96d0adda4644899d079da5cb340944d355b9
Henrik Schmidt <henrik@henrik-schmidt.de>
2016-12-09 10:35:49 +0100

    Adapted to current Kubernetes go client & removed custom code with kubernetes components

--- server.go
+++ server.go
@@ -31,19 +31,16 @@ import (
 	"github.com/golang/glog"
 	"github.com/kubermatic/k8sniff/parser"
 
-	corev1 "k8s.io/client-go/1.4/kubernetes/typed/core/v1"
-	typedv1beta1 "k8s.io/client-go/1.4/kubernetes/typed/extensions/v1beta1"
-	"k8s.io/client-go/1.4/pkg/api"
-	_ "k8s.io/client-go/1.4/pkg/api/install"
-	apiv1 "k8s.io/client-go/1.4/pkg/api/v1"
-	_ "k8s.io/client-go/1.4/pkg/apis/extensions/install"
-	extapiv1beta1 "k8s.io/client-go/1.4/pkg/apis/extensions/v1beta1"
-	"k8s.io/client-go/1.4/pkg/fields"
-	"k8s.io/client-go/1.4/pkg/util/intstr"
-	"k8s.io/client-go/1.4/pkg/watch"
-	"k8s.io/client-go/1.4/rest"
-	"k8s.io/client-go/1.4/tools/cache"
-	"k8s.io/client-go/1.4/tools/clientcmd"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	"k8s.io/client-go/pkg/watch"
+	"k8s.io/client-go/pkg/util/intstr"
+	"k8s.io/client-go/pkg/runtime"
+	"k8s.io/client-go/pkg/util/wait"
 )
 
 const (
@@ -110,6 +107,99 @@ func (p *Proxy) Update(c *Config) error {
 	return nil
 }
 
+func (c *Config) UpdateServers() error {
+	class := c.Kubernetes.IngressClass
+	if class == "" {
+		class = "k8sniff"
+	}
+
+	c.lock.Lock()
+	serverForBackend := func(ing *v1beta1.Ingress, backend *v1beta1.IngressBackend) (*Server, error) {
+		obj, found, err := c.serviceStore.GetByKey(fmt.Sprintf("%s/%s", ing.Namespace, backend.ServiceName))
+		if err != nil {
+			return nil, err
+		}
+		if !found {
+			return nil, fmt.Errorf("service %s/%s not found", ing.Namespace, backend.ServiceName)
+		}
+		svc := obj.(*v1.Service)
+		var port int
+		if backend.ServicePort.Type == intstr.String {
+			for _, p := range svc.Spec.Ports {
+				if p.Name == backend.ServicePort.StrVal {
+					port = int(p.Port)
+					break
+				}
+			}
+			if port == 0 {
+				return nil, fmt.Errorf("port %q of service %s/%s not found", backend.ServicePort.StrVal, svc.Namespace, svc.Name)
+			}
+		} else {
+			port = int(backend.ServicePort.IntVal)
+		}
+		return &Server{
+			Host: svc.Spec.ClusterIP,
+			// TODO: support string values:
+			Port: port,
+		}, nil
+	}
+
+	c.Servers = []Server{}
+	iList := c.ingressStore.List()
+	for _, i := range iList {
+		i := i.(*v1beta1.Ingress)
+		name := fmt.Sprintf("%s/%s", i.Namespace, i.Name)
+		if i.Annotations[ingressClassKey] != class {
+			glog.Errorf("Skipping ingress %s due to missing annotation. Expected %s=%s Got %s=%s", name, ingressClassKey, class, ingressClassKey, i.Annotations[ingressClassKey])
+			continue
+		}
+
+		if i.Spec.Backend != nil {
+			s, err := serverForBackend(i, i.Spec.Backend)
+			if err != nil {
+				glog.Errorf("Ingress %s error with default backend, skipping: %v", name, err)
+			} else {
+				s.Default = true
+				glog.V(4).Infof("Adding default backend -> %s:%d", s.Host, s.Port)
+				c.Servers = append(c.Servers, *s)
+			}
+		}
+		for _, r := range i.Spec.Rules {
+			if r.HTTP == nil {
+				glog.Errorf("Ingress %s error with rule, skipping: http must be set", name)
+				continue
+			}
+			for _, p := range r.HTTP.Paths {
+				if p.Path != "" && p.Path != "/" {
+					glog.Errorf("Ingress %s error with rule, skipping: path is not empty", name)
+					continue
+				}
+				s, err := serverForBackend(i, &p.Backend)
+				if err != nil {
+					glog.Errorf("Ingress %s error with rule %q path %q, skipping: %v", name, r.Host, p.Path, err)
+					continue
+				}
+				s.Names = []string{r.Host}
+				glog.V(4).Infof("Adding backend %q -> %s:%d", r.Host, s.Host, s.Port)
+				c.Servers = append(c.Servers, *s)
+			}
+		}
+	}
+	c.lock.Unlock()
+
+	glog.V(2).Infof("Updating proxy configuration")
+	err := c.proxy.Update(c)
+	if err != nil {
+		glog.Errorf("Error updating proxy: %v", err)
+		// TODO: add backoff logic
+		time.Sleep(time.Second)
+	} else {
+		glog.V(2).Infof("Proxy configuration update done")
+	}
+
+	return nil
+}
+
 func (c *Config) Serve() error {
 	glog.V(1).Infof("Listening on %s:%d", c.Bind.Host, c.Bind.Port)
 	listener, err := net.Listen("tcp", fmt.Sprintf(
@@ -119,8 +209,8 @@ func (c *Config) Serve() error {
 		return err
 	}
 
-	proxy := Proxy{}
-	err = proxy.Update(c)
+	c.proxy = &Proxy{}
+	err = c.proxy.Update(c)
 	if err != nil {
 		return err
 	}
@@ -142,169 +232,79 @@ func (c *Config) Serve() error {
 			}
 		}
 
-		extclient := typedv1beta1.NewForConfigOrDie(rcfg)
-		client := corev1.NewForConfigOrDie(rcfg)
+		client := kubernetes.NewForConfigOrDie(rcfg)
 
-		// trigger to update the proxy
-		updateTrigger := make(chan struct{}, 1)
-
-		// watch services
-		services := NotifyingStore{
-			Store: cache.NewStore(cache.MetaNamespaceKeyFunc),
-			NotifyFunc: func() {
-				select {
-				case updateTrigger <- struct{}{}:
-				default:
-				}
+		c.ingressStore, c.ingressController = cache.NewInformer(
+			&cache.ListWatch{
+				ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
+					return client.Extensions().Ingresses(v1.NamespaceAll).List(options)
+				},
+				WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
+					return client.Extensions().Ingresses(v1.NamespaceAll).Watch(options)
+				},
 			},
-		}
-		lw := cache.NewListWatchFromClient(client, "services", "", fields.Everything())
-		refl := cache.NewReflector(lw, &apiv1.Service{}, &services, time.Minute)
-		refl.Run()
+			&v1beta1.Ingress{},
+			5 * time.Minute,
+			cache.ResourceEventHandlerFuncs{
+				AddFunc: func(obj interface{}) {
+					i := obj.(*v1beta1.Ingress)
+					glog.V(4).Infof("Adding ingress %s/%s", i.Namespace, i.Name)
+					c.UpdateServers()
+				},
+				UpdateFunc: func(old, cur interface{}) {
+					i := cur.(*v1beta1.Ingress)
+					glog.V(4).Infof("Updating ingress %s/%s", i.Namespace, i.Name)
+					c.UpdateServers()
+				},
+				DeleteFunc: func(obj interface{}) {
+					i := obj.(*v1beta1.Ingress)
+					glog.V(4).Infof("Deleting ingress %s/%s", i.Namespace, i.Name)
+					c.UpdateServers()
+				},
+			},
+		)
+
+		c.serviceStore, c.serviceController = cache.NewInformer(
+			&cache.ListWatch{
+				ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
+					return client.Services(v1.NamespaceAll).List(options)
+				},
+				WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
+					return client.Services(v1.NamespaceAll).Watch(options)
+				},
+			},
+			&v1.Service{},
+			5 * time.Minute,
+			cache.ResourceEventHandlerFuncs{
+				AddFunc: func(obj interface{}) {
+					s := obj.(*v1.Service)
+					glog.V(4).Infof("Adding service %q", s.Name)
+					c.UpdateServers()
+				},
+				UpdateFunc: func(old, cur interface{}) {
+					s := cur.(*v1.Service)
+					glog.V(4).Infof("Updating service %q", s.Name)
+					c.UpdateServers()
+				},
+				DeleteFunc: func(obj interface{}) {
+					s := obj.(*v1.Service)
+					glog.V(4).Infof("Deleting service %q", s.Name)
+					c.UpdateServers()
+				},
+			},
+		)
 
 		// wait until services are ready
 		glog.V(1).Infof("Waiting for service store to be ready")
+		go c.serviceController.Run(wait.NeverStop)
 		for {
-			if refl.LastSyncResourceVersion() != "" {
+			if c.serviceController.HasSynced() {
 				break
 			}
 			time.Sleep(time.Millisecond * 200)
 		}
 
-		// watch ingresses
-		ingresses := map[string]*extapiv1beta1.Ingress{}
-		lock := sync.Mutex{}
-		class := c.Kubernetes.IngressClass
-		if class == "" {
-			class = "k8sniff"
-		}
-		go func() {
-			for {
-				w, err := extclient.Ingresses("").Watch(api.ListOptions{})
-				if err != nil {
-					glog.Errorf("Ingress watch error: %v", err)
-					// TODO: add backoff logic
-					time.Sleep(time.Second)
-					continue
-				}
-				evs := w.ResultChan()
-
-			EventLoop:
-				for ev := range evs {
-					i := ev.Object.(*extapiv1beta1.Ingress)
-					if i != nil && i.Annotations[ingressClassKey] != class {
-						continue
-					}
-					switch ev.Type {
-					case watch.Added, watch.Modified:
-						glog.V(5).Infof("event %s for %s/%s", ev.Type, i.Namespace, i.Name)
-						lock.Lock()
-						ingresses[i.Namespace+"/"+i.Name] = i
-						lock.Unlock()
-					case watch.Deleted:
-						glog.V(5).Infof("event %s for %s/%s", ev.Type, i.Namespace, i.Name)
-						lock.Lock()
-						delete(ingresses, i.Namespace+"/"+i.Name)
-						lock.Unlock()
-					case watch.Error:
-						if i != nil {
-							glog.V(5).Infof("event %s for %s/%s", ev.Type, i.Namespace, i.Name)
-						}
-						w.Stop()
-						break EventLoop
-					}
-					select {
-					case updateTrigger <- struct{}{}:
-					default:
-					}
-				}
-
-				// TODO: add backoff logic
-				time.Sleep(time.Second)
-			}
-		}()
-
-		go func() {
-			for range updateTrigger {
-				lock.Lock()
-
-				serverForBackend := func(ing *extapiv1beta1.Ingress, backend *extapiv1beta1.IngressBackend) (*Server, error) {
-					obj, found, err := services.GetByKey(fmt.Sprintf("%s/%s", ing.Namespace, backend.ServiceName))
-					if err != nil {
-						return nil, err
-					}
-					if !found {
-						return nil, fmt.Errorf("service %s/%s not found", ing.Namespace, backend.ServiceName)
-					}
-					svc := obj.(*apiv1.Service)
-					var port int
-					if backend.ServicePort.Type == intstr.String {
-						for _, p := range svc.Spec.Ports {
-							if p.Name == backend.ServicePort.StrVal {
-								port = int(p.Port)
-								break
-							}
-						}
-						if port == 0 {
-							return nil, fmt.Errorf("port %q of service %s/%s not found", backend.ServicePort.StrVal, svc.Namespace, svc.Name)
-						}
-					} else {
-						port = int(backend.ServicePort.IntVal)
-					}
-					return &Server{
-						Host: svc.Spec.ClusterIP,
-						// TODO: support string values:
-						Port: port,
-					}, nil
-				}
-
-				c.Servers = []Server{}
-				for _, i := range ingresses {
-					if i.Spec.Backend != nil {
-						s, err := serverForBackend(i, i.Spec.Backend)
-						if err != nil {
-							glog.Errorf("Ingress %s/%s error with default backend, skipping: %v", i.Namespace, i.Name, err)
-						} else {
-							s.Default = true
-							glog.V(4).Infof("Adding default backend -> %s:%d", s.Host, s.Port)
-							c.Servers = append(c.Servers, *s)
-						}
-					}
-					for _, r := range i.Spec.Rules {
-						if r.HTTP == nil {
-							glog.Errorf("Ingress %s/%s error with rule, skipping: http must be set", i.Namespace, i.Name)
-							continue
-						}
-						for _, p := range r.HTTP.Paths {
-							if p.Path != "" && p.Path != "/" {
-								glog.Errorf("Ingress %s/%s error with rule, skipping: %v", i.Namespace, i.Name, err)
-								continue
-							}
-							s, err := serverForBackend(i, &p.Backend)
-							if err != nil {
-								glog.Errorf("Ingress %s/%s error with rule %q path %q, skipping: %v", i.Namespace, i.Name, r.Host, p.Path, err)
-								continue
-							}
-							s.Names = []string{r.Host}
-							glog.V(4).Infof("Adding backend %q -> %s:%d", r.Host, s.Host, s.Port)
-							c.Servers = append(c.Servers, *s)
-						}
-					}
-				}
-				lock.Unlock()
-
-				glog.V(2).Infof("Updating proxy configuration")
-				err := proxy.Update(c)
-				if err != nil {
-					glog.Errorf("Error updating proxy: %v", err)
-					// TODO: add backoff logic
-					time.Sleep(time.Second)
-				} else {
-					glog.V(2).Infof("Proxy configuration update done")
-				}
-
-			}
-		}()
+		go c.ingressController.Run(wait.NeverStop)
 	}
 
 	for {
@@ -317,7 +317,7 @@ func (c *Config) Serve() error {
 			conn.RemoteAddr(),
 			conn.LocalAddr(),
 		)
-		go proxy.Handle(conn)
+		go c.proxy.Handle(conn)
 	}
 }
 
@@ -390,5 +390,3 @@ func Copycat(client, server net.Conn) {
 	}
 
 }
-
-// vim: foldmethod=marker
