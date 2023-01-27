64d38761f0d9a8d2486f29c5c99c23b2f7d38972
Thomas Dell <tdell408@gmail.com>
2023-01-27 10:10:30 +0000

    Update due to API removals in Kubernetes v1.22

--- server.go
+++ server.go
@@ -30,17 +30,18 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"context"
 
 	"github.com/platform9/cnxmd/pkg/cnxmd"
 
 	"github.com/golang/glog"
-	"github.com/kubermatic/k8sniff/metrics"
-	"github.com/kubermatic/k8sniff/parser"
+	"github.com/platform9/k8sniff/metrics"
+	"github.com/platform9/k8sniff/parser"
     "k8s.io/api/core/v1"
-	"k8s.io/api/extensions/v1beta1"
+	netv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/intstr"
+	/* "k8s.io/apimachinery/pkg/util/intstr" */
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
@@ -127,47 +128,33 @@ func (c *Config) UpdateServers(reason string) error {
 		class = "k8sniff"
 	}
 
-	serverForBackend := func(ing *v1beta1.Ingress, backend *v1beta1.IngressBackend) (*Server, error) {
-		obj, found, err := c.serviceStore.GetByKey(fmt.Sprintf("%s/%s", ing.Namespace, backend.ServiceName))
+	serverForBackend := func(ing *netv1.Ingress, backend *netv1.IngressBackend) (*Server, error) {
+		obj, found, err := c.serviceStore.GetByKey(fmt.Sprintf("%s/%s", ing.Namespace, backend.Service.Name))
 		if err != nil {
 			return nil, err
 		}
 		if !found {
-			return nil, fmt.Errorf("service %s/%s not found", ing.Namespace, backend.ServiceName)
+			return nil, fmt.Errorf("service %s/%s not found", ing.Namespace, backend.Service.Name)
 		}
 		svc := obj.(*v1.Service)
-		var port int
-		if backend.ServicePort.Type == intstr.String {
-			for _, p := range svc.Spec.Ports {
-				if p.Name == backend.ServicePort.StrVal {
-					port = int(p.Port)
-					break
-				}
-			}
-			if port == 0 {
-				return nil, fmt.Errorf("port %s of service %s/%s not found", backend.ServicePort.StrVal, svc.Namespace, svc.Name)
-			}
-		} else {
-			port = int(backend.ServicePort.IntVal)
-		}
 		return &Server{
 			Host: svc.Spec.ClusterIP,
-			Port: port,
+			Port: int(backend.Service.Port.Number),
 		}, nil
 	}
 
 	var servers []Server
 	ingressList := c.ingressStore.List()
 	for _, i := range ingressList {
-		i := i.(*v1beta1.Ingress)
+		i := i.(*netv1.Ingress)
 		name := fmt.Sprintf("%s/%s", i.Namespace, i.Name)
 		if i.Annotations[ingressClassKey] != class {
 			glog.V(6).Infof("Skipping ingress %s due to missing annotation. Expected %s=%s Got %s=%s", name, ingressClassKey, class, ingressClassKey, i.Annotations[ingressClassKey])
 			continue
 		}
 
-		if i.Spec.Backend != nil {
-			s, err := serverForBackend(i, i.Spec.Backend)
+		if i.Spec.DefaultBackend != nil {
+			s, err := serverForBackend(i, i.Spec.DefaultBackend)
 			if err != nil {
 				metrics.IncErrors(metrics.Error)
 				glog.V(0).Infof("Ingress %s error with default backend, skipping: %v", name, err)
@@ -286,24 +273,24 @@ func (c *Config) Serve(stopCh chan struct{}) error {
 		c.ingressStore, c.ingressController = cache.NewInformer(
 			&cache.ListWatch{
 				ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-					return c.Kubernetes.Client.ExtensionsV1beta1().Ingresses("").List(options)
+					return c.Kubernetes.Client.NetworkingV1().Ingresses("").List(context.TODO(), options)
 				},
 				WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-					return c.Kubernetes.Client.ExtensionsV1beta1().Ingresses("").Watch(options)
+					return c.Kubernetes.Client.NetworkingV1().Ingresses("").Watch(context.TODO(), options)
 				},
 			},
-			&v1beta1.Ingress{},
+			&netv1.Ingress{},
 			30*time.Minute,
 			cache.ResourceEventHandlerFuncs{
 				AddFunc: func(obj interface{}) {
-					ing := obj.(*v1beta1.Ingress)
+					ing := obj.(*netv1.Ingress)
 					msg := fmt.Sprintf("ingress added: %s/%s", ing.Namespace, ing.Name)
 					glog.V(3).Infof(msg)
 					go c.TriggerUpdate(msg)
 				},
 				UpdateFunc: func(old, cur interface{}) {
-					oldIng := old.(*v1beta1.Ingress)
-					ing := cur.(*v1beta1.Ingress)
+					oldIng := old.(*netv1.Ingress)
+					ing := cur.(*netv1.Ingress)
 					if reflect.DeepEqual(oldIng, ing) {
 						glog.V(3).Infof("ignoring spurious update for ingress %s/%s",
 							ing.Namespace, ing.Name)
@@ -315,7 +302,7 @@ func (c *Config) Serve(stopCh chan struct{}) error {
 					go c.TriggerUpdate(msg)
 				},
 				DeleteFunc: func(obj interface{}) {
-					ing := obj.(*v1beta1.Ingress)
+					ing := obj.(*netv1.Ingress)
 					msg := fmt.Sprintf("ingress deleted: %s/%s", ing.Namespace, ing.Name)
 					glog.V(3).Infof(msg)
 					go c.TriggerUpdate(msg)
@@ -326,10 +313,10 @@ func (c *Config) Serve(stopCh chan struct{}) error {
 		c.serviceStore, c.serviceController = cache.NewInformer(
 			&cache.ListWatch{
 				ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-					return c.Kubernetes.Client.Services("").List(options)
+					return c.Kubernetes.Client.CoreV1().Services("").List(context.TODO(), options)
 				},
 				WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-					return c.Kubernetes.Client.Services("").Watch(options)
+					return c.Kubernetes.Client.CoreV1().Services("").Watch(context.TODO(), options)
 				},
 			},
 			&v1.Service{},
